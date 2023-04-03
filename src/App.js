63613165f8c4e350c18fc27d655223a7aebe53ad
Antonijo Galic <antonijodev@gmail.com>
2023-04-04 00:48:00 +0200

    Implementing authentication and firebase together with sign up and sign in page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import Authentication from './routes/authentication/authentication.component';
 import Home from './routes/home/home.components';
 import Navigation from './routes/navigation/navigation.component';
 
@@ -11,7 +12,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
